8a15df66f4156fc4960e02b5be7b4ec2fe9c0dd7
arvee0220 <rowellverdejo@gmail.com>
2024-06-04 10:13:40 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./routes/Header/Header.component";
+import Navigation from "./routes/Navigation/Navigation.component";
+
+const App = () => {
+	return (
+		<>
+			<Navigation />
+			<Header />
+		</>
+	);
+};
+
+export default App;
---
+++ src/routes/Header/Header.component.jsx
@@ -0,0 +1,28 @@
+import { images } from "../../constants";
+import SubHeading from "../../components/Subheading/SubHeading.component";
+import "./Header.styles.component.scss";
+
+const Header = () => {
+	return (
+		<div className="app__header app__wrapper section__padding" id="home">
+			<div className="app__wrapper_info">
+				<SubHeading title="Chase the new flavour" />
+				<h1 className="app__header-h1">The Key To Fine Dining</h1>
+				<p className="p__opensans" style={{ margin: "2rem 0" }}>
+					Rated as one of the best fine dine restaurant in the Philippines, Maison De Luxe
+					currently has twenty-one locations worldwide. We pride ourselves in our renowned
+					A5 Kobe steaks, fresh seafood and award-winning wine cellar.
+				</p>
+				<button type="button" className="custom__button">
+					Explore Menu
+				</button>
+			</div>
+
+			<div className="app__wrapper_img">
+				<img src={images.welcome} alt="header_img" />
+			</div>
+		</div>
+	);
+};
+
+export default Header;
